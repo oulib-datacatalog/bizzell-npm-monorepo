b50a39d5c0855fab468782d61f42f0c9486deb9a
HeathWil <Heather.o.wilson@ou.edu>
2018-11-29 21:22:39 -0600

    Checkbox changes

---
+++ packages/tempest/src/Checkbox.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+type Props = {
+  label?: string
+  defaultChecked?: boolean
+  checkedIcon?: any
+  uncheckedIcon?: any
+}
+
+const types = ['checkbox']
+
+export default class Checkbox extends React.Component<Props, {}> {
+  private textInput!: HTMLInputElement
+
+  state = {
+    isChecked: false,
+  }
+
+  handleChange = () => {
+    const isInputChecked = this.textInput.checked
+    this.setState({ isChecked: isInputChecked })
+  }
+
+  render() {
+    const { label } = this.props
+    const { isChecked } = this.state
+
+    return (
+      <div className="checkbox">
+        <label>
+          <input
+            type="checkbox"
+            value={label}
+            checked={isChecked}
+            onChange={this.handleChange}
+          />
+
+          {label}
+        </label>
+      </div>
+    )
+  }
+}

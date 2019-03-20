5c03980c6bbfe3186cc751c42d6c3b63deaaea5b
HeathWil <Heather.o.wilson@ou.edu>
2019-03-19 20:35:15 -0500

    Component rendering tests
    component rendering test fixes
    
    component rendering test fixes

--- packages/tempest/src/Card.tsx
+++ packages/tempest/src/Card.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Component } from 'react'
 import styles from './Card.css'
 import { joinNames, ClassNames } from './classNames'
 import { Text } from './Text'
---
+++ packages/tempest/styleMock.ts
@@ -0,0 +1,4 @@
+export default {}
+//The above is necessary due to Jest (our testing feature)
+// being unable to process css. This is an empty class directing
+//jest, when looking for css requirements, to be redirected here. 

8eae81342919101c27d6d44f033a5c6e03f725cb
DuncanWalter <dwalter@barlingarts.com>
2019-04-01 19:18:08 -0500

    making sure package.json crap is in order

--- packages/tempest/src/Checkbox.test.tsx
+++ packages/tempest/src/Checkbox.test.tsx
@@ -1,18 +1,18 @@
-import {cleanup} from 'react-testing-library'
-import {Checkbox} from './Checkbox'
+import { cleanup } from 'react-testing-library'
+import { Checkbox } from './Checkbox'
 import React from 'react'
-import  Enzyme from 'enzyme'
+import Enzyme from 'enzyme'
 import { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-Enzyme.configure({ adapter: new Adapter()})
+
+Enzyme.configure({ adapter: new Adapter() })
 
 afterEach(cleanup)
 
-describe("Button component test", () => {
-
-    it('Should Render Button', () => {
-        const spy = jest.spyOn(Checkbox.prototype,'render' );
-        mount(<Checkbox/>);
-        expect(Checkbox.prototype.render).toHaveBeenCalledTimes(1);
-    });
-});
+describe('Button component test', () => {
+  it('Should Render Button', () => {
+    jest.spyOn(Checkbox.prototype, 'render')
+    mount(<Checkbox />)
+    expect(Checkbox.prototype.render).toHaveBeenCalledTimes(1)
+  })
+})

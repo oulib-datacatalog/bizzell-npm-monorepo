b9cb895da825a5650922fb6a044665e59b2453da
Tyrone Lu <tyronelu99@gmail.com>
2020-03-10 11:55:15 -0500

    basic input form for searching

--- packages/template/src/components/Search.tsx
+++ packages/template/src/components/Search.tsx
@@ -2,7 +2,16 @@ import React, { Component, Fragment } from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 import { RouteComponentProps } from 'react-router-dom'
 import { Navigation } from './Navigation'
-import { CardHeader, CardContent, Card, Text, Button } from '@bizzell/tempest'
+import {
+  CardHeader,
+  CardContent,
+  Card,
+  Text,
+  Button,
+  Checkbox,
+  List,
+} from '@bizzell/tempest'
+import * as R from 'ramda'
 
 type MyState = {
   src_lists: Array<string>
@@ -10,11 +19,22 @@ type MyState = {
   error: any
   isLoaded: boolean
   results: any
+  searchPath: string
+  searchValue: string
+  searchDelim: string
+  searchResults: Array<string>
 }
+
 export class Search extends Component<{}, MyState> {
   constructor(props: any) {
     super(props)
+
     this.handleSelect = this.handleSelect.bind(this)
+    this.handleChangeSearchVal = this.handleChangeSearchVal.bind(this)
+    this.delimOnChange = this.delimOnChange.bind(this)
+    this.searchClick = this.searchClick.bind(this)
+    this.searchOnChange = this.searchOnChange.bind(this)
+
     this.state = {
       src_lists: [
         '../../json/api_catalog_data_catalog_digital_objects.json',
@@ -25,6 +45,10 @@ export class Search extends Component<{}, MyState> {
       error: null,
       isLoaded: false,
       results: [],
+      searchPath: '',
+      searchValue: '',
+      searchDelim: '',
+      searchResults: [],
     }
   }
 
@@ -45,13 +69,48 @@ export class Search extends Component<{}, MyState> {
         this.setState({ src: url, results: data, isLoaded: true })
       })
   }
+
+  searchClick(event: any) {
+    //Split the search query by a delimiter
+    let { results, searchPath, searchDelim, searchValue } = this.state
+    let delim = searchDelim
+    if (searchDelim.length == 0) {
+      delim = ' '
+    }
+    let nodePath = searchPath.split(delim)
+
+    let found = R.pathEq(nodePath, searchValue)
+    let x = R.filter(found, results)
+    this.setState({ searchResults: x })
+  }
+
+  searchOnChange(event: any) {
+    //Dont allow space as first character
+    if (this.state.searchPath.length == 0) {
+      if (event.target.value != ' ') {
+        this.setState({ searchPath: event.target.value })
+      }
+    } else {
+      this.setState({ searchPath: event.target.value })
+    }
+  }
+
+  delimOnChange(event: any) {
+    this.setState({ searchDelim: event.target.value })
+  }
+
+  handleChangeSearchVal(event: any) {
+    let value = event.target.value
+
+    this.setState({ searchValue: value })
+  }
   //Args for path will be hierarcical. EX:['locations', 'norfile', 'exists']
   //Item can contain the arg and other characters. search abc hit: qwioemwoqabcqowpewq
   //Character case does not matter
   //Src can be a drop down or user can type in the src
   //Use Ramda for path finding and filtering
   render() {
-    let { src_lists, isLoaded, results } = this.state
+    let { src_lists, isLoaded, searchResults } = this.state
     if (!isLoaded) {
       return <div>Loading...</div>
     } else {
@@ -60,11 +119,43 @@ export class Search extends Component<{}, MyState> {
           <Navigation />
           <Card>
             <CardContent>
-              <select value={this.state.src} onChange={this.handleSelect}>
-                {src_lists.map((item: any) => (
-                  <option key={item}>{item}</option>
-                ))}
-              </select>
+              <div id="search">
+                <div id="src_select">
+                  <select value={this.state.src} onChange={this.handleSelect}>
+                    {src_lists.map((item: any) => (
+                      <option key={item}>{item}</option>
+                    ))}
+                  </select>
+                </div>
+
+                <div id="search_criteria_input">
+                  <label>Paths</label>
+                  <input
+                    type="text"
+                    name="args_input"
+                    value={this.state.searchPath}
+                    onChange={this.searchOnChange}
+                  />
+                  <label>Delim</label>
+                  <input
+                    type="text"
+                    name="delim"
+                    value={this.state.searchDelim}
+                    onChange={this.delimOnChange}
+                  />
+                  <label>Value</label>
+                  <input
+                    type="text"
+                    name="value"
+                    onChange={this.handleChangeSearchVal}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Search"
+                  onClick={this.searchClick}
+                />
+              </div>
             </CardContent>
           </Card>
         </Fragment>
